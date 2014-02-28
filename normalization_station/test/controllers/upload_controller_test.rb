632adec403bd21b2e2016b043b8e14dd84dc4548
John F Schank III <jschank@mac.com>
2014-02-28 02:03:43 -0500

    updated test

--- normalization_station/test/controllers/upload_controller_test.rb
+++ normalization_station/test/controllers/upload_controller_test.rb
@@ -7,7 +7,11 @@ class UploadControllerTest < ActionController::TestCase
   end
 
   test "should post upload" do
-    old_purchaser_count = Purchaser.count
+    initial_purchaser_count = Purchaser.count
+    initial_item_count = Item.count
+    initial_merchant_count = Merchant.count
+    initial_purchases_count = Purchase.count
+
     tsv_file = fixture_file_upload('files/example_input.tab', 'application/octet-stream')
     post :upload, :data_file => {:file => tsv_file}
 
@@ -15,8 +19,17 @@ class UploadControllerTest < ActionController::TestCase
     assert_equal 95.0, assigns(:gross_revenue)
     assert_equal flash[:notice], "File upload succeeded!"
 
-    assert_equal 0, old_purchaser_count
+    assert_equal 0, initial_purchaser_count
     assert_equal 3, Purchaser.count
+
+    assert_equal 0, initial_item_count
+    assert_equal 3, Item.count
+
+    assert_equal 0, initial_merchant_count
+    assert_equal 3, Merchant.count
+
+    assert_equal 0, initial_purchases_count
+    assert_equal 4, Purchase.count
 
     assert_response :success
   end
