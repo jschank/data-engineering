a4230a22ae2699079bd8c3830af27a7c600919a6
John F Schank III <jschank@mac.com>
2014-02-27 23:28:24 -0500

    initial generated files

---
+++ normalization_station/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NormalizationStation::Application.config.secret_key_base = '02ada920e1ed1559b127270d032768c334f5ba49839aa3a8687407b1c24fb151fe29f4621dafe437e7f2ea946ba8739b26c61251752ae5d52620ca5e4a2a1ab9'
---
+++ normalization_station/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+NormalizationStation::Application.config.session_store :cookie_store, key: '_normalization_station_session'
