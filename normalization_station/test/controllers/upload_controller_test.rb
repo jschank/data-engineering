315aedb82588e93cd32059d3dce95c83542dcf81
John F Schank III <jschank@mac.com>
2014-02-28 01:59:53 -0500

    fixed test - messed up by fixtures

--- normalization_station/test/controllers/upload_controller_test.rb
+++ normalization_station/test/controllers/upload_controller_test.rb
@@ -7,13 +7,16 @@ class UploadControllerTest < ActionController::TestCase
   end
 
   test "should post upload" do
-    # tsv_file = fixture_file_upload('files/example_input.tab', 'application/octet-stream')
+    old_purchaser_count = Purchaser.count
     tsv_file = fixture_file_upload('files/example_input.tab', 'application/octet-stream')
     post :upload, :data_file => {:file => tsv_file}
 
     assert_equal "example_input.tab", assigns(:uploaded_file_name)
     assert_equal 95.0, assigns(:gross_revenue)
     assert_equal flash[:notice], "File upload succeeded!"
+
+    assert_equal 0, old_purchaser_count
+    assert_equal 3, Purchaser.count
 
     assert_response :success
   end
